2856e38dbadc5ee1db8610db5e02e75e132f3450
Kyle <kyle@Efm-MacBook-Pro.local>
2025-07-15 00:36:49 -0700

    describe 'Price' field

--- ft_engine.py
+++ ft_engine.py
@@ -12,8 +12,13 @@ url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-ML
 
 os.makedirs("data", exist_ok=True)
 path = "data/airline_data.xlsx"
+print(utils.checkIfAssetExists(path))
 if not utils.checkIfAssetExists(path):
     utils.download(url, path)
 
 data = pd.read_excel(path)
 print(data.head())
+
+print(data.info())
+# Provides stat information about numerical values (only have 1: "Price")
+print(data.describe())
