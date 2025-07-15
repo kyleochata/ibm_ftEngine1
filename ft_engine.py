a34733cdd84468080072ca26956a74209db64375
Kyle <kyle@Efm-MacBook-Pro.local>
2025-07-15 13:15:15 -0700

    heatmap generated

--- ft_engine.py
+++ ft_engine.py
@@ -90,3 +90,58 @@ data1['Duration_minutes'] = dur_minutes
 data1.loc[:, 'Duration_hours'] *= 60
 data1['Duration_Total_mins'] = data1['Duration_hours'] + data1['Duration_minutes']
 print(data1.loc[:,['Duration_hours','Duration_minutes','Duration_Total_mins']].head())
+
+# Transform 'Departure', 'Arrival'
+data1["Dep_Hour"] = pd.to_datetime(data1["Dep_Time"]).dt.hour
+data1["Dep_Min"] = pd.to_datetime(data1["Dep_Time"]).dt.minute
+data1["Arrival_Hour"] = pd.to_datetime(data1["Arrival_Time"]).dt.hour
+data1["Arrival_Min"] = pd.to_datetime(data1["Arrival_Time"]).dt.minute
+
+# Split Departure and Arrival times into time zones
+data1['dep_timezone'] = pd.cut(data1["Dep_Hour"], [0,6,12,18,24], labels=['Night', 'Morning', 'Afternoon', 'Evening'])
+print(data1["dep_timezone"])
+data1['arrival_timezone'] = pd.cut(data1["Arrival_Hour"], [0,6,12,18,24], labels=['Night', 'Morning', 'Afternoon', 'Evening'])
+print(f"arrivals\n {data1["arrival_timezone"]}")
+
+timezone_mapping = {'Night': 0, 'Morning': 1, 'Afternoon': 2, 'Evening': 3}
+data1['dep_timezone_encoded'] = data1["dep_timezone"].map(timezone_mapping)
+data1['arrival_timezone_encoded'] = data1["arrival_timezone"].map(timezone_mapping)
+
+# Transform 'Date_of_Journey'. Og dtype is an object that MLs can't use yet
+# create a new month column, extract the month from  the date_of_journey
+data1['Month'] = pd.to_datetime(data1["Date_of_Journey"], format="%d/%m/%Y").dt.month
+# extract day from date_of_journey
+data1['Day'] = pd.to_datetime(data1["Date_of_Journey"], format="%d/%m/%Y").dt.day
+# extract year from date_of_journey
+data1['Year'] = pd.to_datetime(data1["Date_of_Journey"], format="%d/%m/%Y").dt.year
+# extract the day of the week from date_of_journey
+data1["day_of_week"] = pd.to_datetime(data1["Date_of_Journey"], format="%d/%m/%Y").dt.day_name()
+
+# Feature Selection: choose attributes which best explain the relationship of the independent variables with respect to the target variable: "Price"
+# Building a heatmap and calculating the correlation coefficients scores are most common
+
+# Select only relevant and newly transformed variables; exclude 'Route', 'Additional_info', and all original categorical variables.
+# Place into a 'new_data' DataFrame
+print(data1.columns)
+
+heatmap_feature_cols = ['Total_Stops', 'Airline_Air Asia',
+       'Airline_Air India', 'Airline_GoAir', 'Airline_IndiGo',
+       'Airline_Jet Airways', 'Airline_Multiple carriers', 'Airline_SpiceJet',
+       'Airline_Trujet', 'Airline_Vistara', 'Source_Banglore',
+       'Source_Chennai', 'Source_Delhi', 'Source_Kolkata', 'Source_Mumbai',
+       'Destination_Banglore', 'Destination_Cochin', 'Destination_Delhi',
+       'Destination_Hyderabad', 'Destination_Kolkata', 'Destination_New Delhi',
+       'Duration_hours', 'Duration_minutes', 'Duration_Total_mins', 'Dep_Hour',
+       'Dep_Min', 'dep_timezone_encoded', 'arrival_timezone_encoded', 'Price']
+heatmap_data = data1.loc[:, heatmap_feature_cols]
+
+os.makedirs('plots', exist_ok=True)
+
+plt.figure(figsize=(18,18))
+# .corr() - calculates the pairwise correlation b/w numerical cols in a DataFrame. Outputs a val representing how strong 2 vars are linearly related
+# Pearson correlation coefficient (linear relationship). 1: pos relation; -1: neg relation; 0: no linear correlation
+sns.heatmap(heatmap_data.corr(), annot=True, cmap='RdYlGn')
+path = "plots/heatmap_features.png"
+if not utils.checkIfAssetExists(path):
+    utils.savePlot(path)
+
